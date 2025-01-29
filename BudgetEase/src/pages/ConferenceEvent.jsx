ef8d32641eedd7f1e1f206158e45047d544a8844
Radhi Hudijan <radhif2009@gmail.com>
2025-01-29 23:34:51 +0100

    feat: update ConferenceEvent component with new navbar color and add venue room selection section

--- BudgetEase/src/pages/ConferenceEvent.jsx
+++ BudgetEase/src/pages/ConferenceEvent.jsx
@@ -3,7 +3,7 @@ import React from "react"
 function ConferenceEvent() {
   return (
     <>
-      <navbar className="flex justify-between items-center h-16 bg-blue-900 p-10">
+      <navbar className="flex justify-between items-center h-16 bg-[#123B6A] p-10">
         <div className="font-bold text-yellow-500 text-2xl">
           Conference Expense Planner
         </div>
@@ -33,6 +33,18 @@ function ConferenceEvent() {
           </button>
         </div>
       </navbar>
+
+      {/* {Main container} */}
+      <div className="flex flex-col justify-center items-center p-10">
+        {/*Room Selection section */}
+        <div className="flex flex-col gap-5 justify-center items-center">
+          <div className="flex justify-center items-center bg-[#123B6A] p-5 text-white font-bold w-[900px] text-4xl text-center">
+            <h1>Venue Room Selection </h1>
+          </div>
+          <div> item card</div>
+          <div>Total cost : </div>
+        </div>
+      </div>
     </>
   )
 }
