67800fcc21c0d0313c7ae5f5d38dd222fa16c50a
Radhi Hudijan <radhif2009@gmail.com>
2025-01-29 14:58:59 +0100

    feat: enhance ConferenceEvent component with navigation bar and action button

--- BudgetEase/src/pages/ConferenceEvent.jsx
+++ BudgetEase/src/pages/ConferenceEvent.jsx
@@ -1,7 +1,40 @@
 import React from "react"
 
 function ConferenceEvent() {
-  return <div>ConferenceEvent</div>
+  return (
+    <>
+      <navbar className="flex justify-between items-center h-16 bg-blue-900 p-10">
+        <div className="font-bold text-yellow-500 text-2xl">
+          Conference Expense Planner
+        </div>
+        <div className="flex gap-10 justify-around items-center">
+          <div className="flex gap-5 px-5">
+            <a
+              href="/"
+              className="text-white hover:text-yellow-500 px-3 py-2 rounded"
+            >
+              Venue
+            </a>
+            <a
+              href="/"
+              className="text-white hover:text-yellow-500 px-3 py-2 rounded"
+            >
+              Add-ons
+            </a>
+            <a
+              href="/"
+              className="text-white hover:text-yellow-500 px-3 py-2 rounded"
+            >
+              Meals
+            </a>
+          </div>
+          <button className="bg-yellow-500 hover:bg-yellow-700 text-black font-bold py-3 px-6 rounded uppercase">
+            Show Details
+          </button>
+        </div>
+      </navbar>
+    </>
+  )
 }
 
 export default ConferenceEvent
